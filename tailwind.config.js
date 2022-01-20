4a0f9d6b59648b046bc8d16bc8b51d57a9b0f46a
Chikezie Joachim <56943504+chuddyjoachim@users.noreply.github.com>
2022-01-20 20:19:58 +0100

    add and congured tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#3F403F',
+        'green-light': '#06942A33',
+        'green-dark': '#06942A',
+        light: '#9B9B9B',
+        e5: '#E5E5E5',
+      },
+      borderRadius: {
+        '50px': '50px',
+      },
+    },
+  },
+  plugins: [],
+}
