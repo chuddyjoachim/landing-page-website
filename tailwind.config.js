8912f78b0a556df3955eb9f8ef70a4e0cd37d4a3
Chikezie Joachim <56943504+chuddyjoachim@users.noreply.github.com>
2022-01-21 07:35:54 +0100

    add custom tailwwindcss propertiess and prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       colors: {
@@ -9,12 +7,17 @@ module.exports = {
         'green-light': '#06942A33',
         'green-dark': '#06942A',
         light: '#9B9B9B',
+        'light-1': '#F4F5F9',
         e5: '#E5E5E5',
+        'shadow-1': '#06942A0D',
       },
       borderRadius: {
         '50px': '50px',
       },
+      boxShadow: {
+        'custom-1': '0px 10px 30px 0px rgba(6, 148, 42, 0.05)', //box-shadow: 0px 10px 30px 0px rgba(6, 148, 42, 0.05);
+      },
     },
   },
   plugins: [],
-}
+};
